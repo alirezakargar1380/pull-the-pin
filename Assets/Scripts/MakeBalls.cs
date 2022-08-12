70d734586d08a56f07e5cc987b25f60cf09b9515
alireza kargar <imanesche@gmail.com>
2022-08-12 21:17:18 +0430

    add resourses

--- Assets/Scripts/MakeBalls.cs
+++ Assets/Scripts/MakeBalls.cs
@@ -9,11 +9,7 @@ public class MyClass
     public string playerName;
 }
 
-public class Ob
-{
-    public string playerName;
-    public List<MyClass> classes;
-}
+
 
 public class MakeBalls : MonoBehaviour
 {
@@ -22,13 +18,38 @@ public class MakeBalls : MonoBehaviour
     public GameObject Bomb;
     public float space;
     public List<GameObject> levelObjects;
+    public TextAsset levelsTxt;
+
+    [System.Serializable]
+    public class Coordinate
+    {
+        public float x;
+        public float y;
+    }
+
+    [System.Serializable]
+    public class BallO
+    {
+        public Coordinate startPoint;
+        public int num;
+    }
+
+    [System.Serializable]
+    public class Levels
+    {
+        public BallO[] balls;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Levels MyLevels = new Levels();
+        MyLevels = JsonUtility.FromJson<Levels>(levelsTxt.text);
+        string vv= JsonUtility.ToJson(MyLevels.balls[0]);
+        Debug.Log(vv);
+        Debug.Log(MyLevels.balls[0].startPoint.x);
 
-    MyClass myObject = new MyClass();
+        MyClass myObject = new MyClass();
         myObject.level = 1;
         myObject.timeElapsed = 47.5f;
         myObject.playerName = "Dr Charles Francis";
@@ -45,6 +66,7 @@ public class MakeBalls : MonoBehaviour
         }
 
         
+        // get bomb by Id
         for (int i = 0; i < levelObjects.Count; i++)
         {
             if (levelObjects[i].name == "bomb_1")
