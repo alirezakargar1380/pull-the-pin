ed04415b143199eaed101bbfdac5ce09e8175db3
alireza kargar <imanesche@gmail.com>
2022-08-15 12:52:22 +0430

    working on pin movement when clicked on them

--- Assets/Scripts/PinHandler.cs
+++ Assets/Scripts/PinHandler.cs
@@ -6,22 +6,37 @@ public class PinHandler : MonoBehaviour
 {
     public MakeBalls MakeBallsScript;
     [SerializeField] public string[] BombIds;
+    Vector3 currentPosition;
     private void OnMouseDown()
     {
-        // Debug.Log(gameObject.transform.position.x);
-        // Instantiate(gameObject, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 20, gameObject.transform.position.z), gameObject.transform.rotation);
-        Destroy(gameObject);
-        MakeBallsScript.ExploadeBomb("bomb_0");
+        Debug.Log(gameObject.name);
+        // Instantiate(gameObject, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1, gameObject.transform.position.z), gameObject.transform.rotation);
+        // gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1, gameObject.transform.position.z).normalized * Time.deltaTime * 0.1f;
+
+        // Destroy(gameObject);
+        // MakeBallsScript.ExploadeBomb("bomb_0");
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentPosition = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 distance = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.2f, gameObject.transform.position.z);
+        if (Vector3.Distance(currentPosition, distance) < 5)
+        {
+            Debug.Log("true");
+        } else
+        {
+            Destroy(gameObject);
+            Debug.Log("false");
+            return;
+        }
+        // gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.2f * 2f, gameObject.transform.position.z);
         
+        transform.position = Vector3.MoveTowards(gameObject.transform.position, distance, Time.deltaTime * 2f);
     }
 }
