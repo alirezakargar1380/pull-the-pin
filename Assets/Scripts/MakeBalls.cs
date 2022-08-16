5104b4f707143deea6cfd7bb6b83c31a3b42dca5
alireza kargar <imanesche@gmail.com>
2022-08-17 03:14:12 +0430

    add pin animation

--- Assets/Scripts/MakeBalls.cs
+++ Assets/Scripts/MakeBalls.cs
@@ -51,7 +51,19 @@ public class MakeBalls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        return;
+
+        // PIN
+        GameObject nPin = Instantiate(Pin, new Vector3(0.8f, 10.82f, 16.424f), Quaternion.Euler(new Vector3(-132.89f, 90, -90)));
+        nPin.GetComponent<PinObject>().BombIds = new string[] { "bomb_0" };
+        nPin.GetComponent<PinObject>().pinName = "bla";
+        nPin.GetComponent<PinObject>().Pins = new GameObject[] { nPin };
+
+        GameObject nnPin = Instantiate(Pin, new Vector3(0.8f, 9.82f, 16.424f), Quaternion.Euler(new Vector3(-132.89f, 90, -90)));
+        nnPin.GetComponent<PinObject>().BombIds = new string[] { "bomb_1" };
+        nnPin.GetComponent<PinObject>().pinName = "yu";
+        nnPin.GetComponent<PinObject>().Pins = new GameObject[] { nnPin };
+
         LevelsDetail level = GetLevel();
         
         // make Balls
@@ -75,16 +87,7 @@ public class MakeBalls : MonoBehaviour
         string json = JsonUtility.ToJson(myObject);
         Debug.Log(json);
 
-        // PIN
-        GameObject nPin = Instantiate(Pin, new Vector3(0.8f, 10.82f, 16.424f), Quaternion.Euler(new Vector3(-132.89f, 90, -90)) );
-        nPin.GetComponent<PinObject>().BombIds = new string[] { "bomb_0" };
-        nPin.GetComponent<PinObject>().pinName = "bla";
-        nPin.GetComponent<PinObject>().Pins = new GameObject[] { nPin };        
-        
-        GameObject nnPin = Instantiate(Pin, new Vector3(0.8f, 9.82f, 16.424f), Quaternion.Euler(new Vector3(-132.89f, 90, -90)) );
-        nnPin.GetComponent<PinObject>().BombIds = new string[] { "bomb_1" };
-        nnPin.GetComponent<PinObject>().pinName = "yu";
-        nnPin.GetComponent<PinObject>().Pins = new GameObject[] { nnPin };
+
 
         return;
 
--- Assets/Scripts/PinHandler.cs
+++ Assets/Scripts/PinHandler.cs
@@ -7,6 +7,7 @@ public class PinHandler : MonoBehaviour
     public MakeBalls MakeBallsScript;
     public PinObject pinObjectSource;
     Vector3 currentPosition;
+    public Vector3 distance;
     private void OnMouseDown()
     {
         string pinName = pinObjectSource.GetComponent<PinObject>().pinName;
@@ -21,13 +22,14 @@ public class PinHandler : MonoBehaviour
     void Start()
     {
         currentPosition = gameObject.transform.position;
+        distance = new Vector3(gameObject.transform.position.x + 6f, gameObject.transform.position.y, gameObject.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 distance = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.2f, gameObject.transform.position.z);
-        if (Vector3.Distance(currentPosition, distance) < 5)
+        return;
+        if (Vector3.Distance(currentPosition, distance) < 4)
         {
             // Debug.Log("true");
         } else
@@ -36,8 +38,9 @@ public class PinHandler : MonoBehaviour
             // Debug.Log("false");
             // return;
         }
-        // gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.2f * 2f, gameObject.transform.position.z);
-        
-        // transform.position = Vector3.MoveTowards(gameObject.transform.position, distance, Time.deltaTime * 2f);
+
+        // transform.position = Vector3.MoveTowards(gameObject.transform.position, distance, Time.deltaTime * 1f);
+        // transform.position = Vector3.MoveTowards(gameObject.transform.position, Vector3.Lerp(gameObject.transform.position, distance, 3f), Time.deltaTime * 3f);
+        transform.Translate(distance, Space.World);
     }
 }
