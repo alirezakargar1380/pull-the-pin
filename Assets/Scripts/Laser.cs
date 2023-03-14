4b67073d5149762952164da97e7a5c3a8ff3ad76
alireza kargar <imanesche@gmail.com>
2023-03-14 15:26:31 +0330

    added laser activition cycle

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -5,20 +5,48 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     public GameObject Smoke;
+    public Animator anim;
+    public bool IsLaserActive = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = gameObject.GetComponent<Animator>();
+        
         // Smoke.gameObject.transform.position = new Vector3(0, 0, 0);
         // Debug.Log(gameObject.name);
         Smoke.SetActive(false);
-        // Invoke("DeactiveSmoke", 2);
+        Invoke("ActivePreviewlaser", 5);
     }
 
     public void DeactiveSmoke() {
         Smoke.SetActive(false);
     }
- 
+
+    public void ActivePreviewlaser() {
+        Debug.Log("preview the laser");
+        anim.SetInteger("int", 1);
+        Invoke("DeActivePreviewlaser", 5);
+    }
+
+    public void DeActivePreviewlaser() {
+        Debug.Log("de active the preview laser");
+        anim.SetInteger("int", 2);
+        ActiveLaser();
+    }
+
+    public void ActiveLaser() {
+        Debug.Log("active the laser");
+        anim.SetInteger("int", 5);
+        Invoke("DeActiveLaser", 3);
+    }
+
+    public void DeActiveLaser()
+    {
+        anim.SetInteger("int", 4);
+        Invoke("ActivePreviewlaser", 5);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Smoke.SetActive(true);
